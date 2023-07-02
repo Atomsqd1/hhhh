00177231d47a343571bf1d22ed3bd447e3ce420a
Atomsqd1 <109301854+Atomsqd1@users.noreply.github.com>
2023-07-02 11:00:00 +0600

    Add files via upload

---
+++ Script/PostQuiz/postQuizQues_Expt1.js
@@ -0,0 +1,61 @@
+
+const quiz =[
+	{   //1
+		
+		q:"What effect does an increase in temperature have on the rate of photosynthesis?", // &#960; -> pi &#593; -> alpha
+		options:['Increases the rate','Decreases the rate','Has no effect','Causes the process to stop'],
+		answer:1,
+		image_link:"../../Assets/Images/ImagesForQuiz/img2.JPG"
+	},
+	{	//2
+		q:'Which pigment is most affected by high light intensity?',
+		options:['Chlorophyll a','Chlorophyll b','Carotenoids','Xanthophylls'],
+		answer:0,
+		image_link:""
+	},
+	{	//3
+		q:'At what temperature does photosynthesis generally occur most efficiently in plants?',
+		options:['0°C (32°F)',' 25°C (77°F)','50°C (122°F)','100°C (212°F)'],
+		answer:1,
+		image_link:""
+	},
+	{	//4
+		q:'What is the term used to describe the process where plants close their stomata to prevent excessive water loss during hot and dry conditions?',
+		options:['Transpiration',' Evaporation','Osmosis','Stomatal closure'],
+		answer:3,
+		image_link:""
+	},
+	{	//5
+		q:'How does an increase in light intensity affect the rate of photosynthesis?',
+		options:['Increases the rate','Decreases the rate','Has no effect','Causes the process to stop'],
+		answer:0,
+		image_link:"../../Assets/Images/ImagesForQuiz/img5.JPG"
+	},
+	{	//6
+		q:'Which of the following is an adaptation of plants to high light intensities?',
+		options:['Increased stomatal opening',' Production of more carotenoids','Decreased chlorophyll content','Reduced photosynthetic activity'],
+		answer:1,
+		image_link:"../../Assets/Images/ImagesForQuiz/img1.JPG"
+	},
+	{	//7
+		q:'What happens to the rate of photosynthesis when light becomes limiting?',
+		options:['Increases','Decreases',' Stays constant','Stops completely'],
+		answer:1,
+		image_link:"../../Assets/Images/ImagesForQuiz/img4.JPG"
+	},
+	{	//8
+		q:'How does a decrease in temperature affect the rate of photosynthesis?',
+		options:["Increases the rate","Decreases the rate","Has no effect","Causes the process to stop"],
+		answer:1,
+		image_link:""
+	},
+	{	//9
+		q:'What is the name of the enzyme responsible for carbon fixation in the Calvin cycle?',
+		options:["NADPH reductase","Cytochrome c","ATP synthase","Rubisco"],
+		answer:3,
+		image_link:"../../Assets/Images/ImagesForQuiz/img3.JPG"
+	}
+]
+
+
+
---
+++ Script/PreQuiz/preQuizQues_Expt1.js
@@ -0,0 +1,55 @@
+
+const quiz =[
+	{	//1
+		q:'What is photosynthesis?',
+		options:['The process of breaking down glucose into energy.','The process of converting light energy into chemical energy.','The process of converting carbon dioxide into oxygen.','The process of synthesizing proteins in plants.'],
+		answer:1
+	},
+	{	//2
+		q:'Which pigment is responsible for capturing light energy in photosynthesis?',
+		options:['Chlorophyll.','Carotene.','Xanthophyll.','Anthocyanin.'],
+		answer:0
+	},
+	{	//3
+		q:'Where does photosynthesis primarily occur in a plant?',
+		options:['Roots.','Leaves.','Stems.','Flowers.'],
+		answer:1
+	},
+	{	//4
+		q:'What are the products of photosynthesis?',
+		options:['Carbon dioxide and water.','Glucose and oxygen.','Nitrogen and phosphorus.','ATP and NADPH.'],
+		answer:1
+	},
+	{	//5
+		q:'What is the primary source of carbon dioxide used in photosynthesis?',
+		options:['Atmospheric air.','Soil.','Water.','Other plants.'],
+		answer:0
+	},
+	{	//6
+		q:"What is the role of light in photosynthesis?",
+		options:['It provides energy to split water molecules.','It converts glucose into ATP.','It excites electrons in chlorophyll molecules.','It breaks down carbon dioxide into oxygen and carbon.'],
+		answer:2
+	},
+	{	//7
+		q:"What is the main function of the stomata in leaves during photosynthesis?",
+		options:["Absorbing sunlight.",'Exchanging gases.','Storing glucose.',' Transporting water.'],
+		answer:1 
+	},
+	{	//8
+		q:"What environmental factor directly affects the rate of photosynthesis?",
+		options:['Temperature.','Wind speed.','Soil pH.','Humidity.'],
+		answer:0
+	},
+	{	//9
+		q:"What is the role of water in photosynthesis?",
+		options:['Water serves as a source of oxygen.','Water acts as a reactant in the light-dependent reactions.',' Water provides the energy for glucose synthesis.','Water is responsible for the absorption of carbon dioxide.'],
+		answer:1
+	},
+	{	//10
+		q:"Which wavelengths of light are most effective for photosynthesis?",
+		options:['Ultraviolet (UV) light.','Infrared (IR) light.','Red and blue light.','Green light.'],
+		answer:2
+	},
+]
+
+
